155eadb88258691f54de14057a6bc809533be1dc
nahid <comeback>
2020-12-14 15:39:13 +0600

    dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,33 +1,38 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from '../../components/date'
 
 export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id)
-    return {
-      props: {
-        postData
-      }
-    }
-  }
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = getAllPostIds();
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
